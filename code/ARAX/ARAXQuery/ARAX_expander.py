55bf7ef985c724c94fb2a73921b8988cf7e18777
Amy Glen <49423686+amykglen@users.noreply.github.com>
2024-02-24 12:06:02 -0800

    Tuck ploverdb query duration onto Response

--- code/ARAX/ARAXQuery/ARAX_expander.py
+++ code/ARAX/ARAXQuery/ARAX_expander.py
@@ -779,7 +779,9 @@ class ARAXExpander:
 
         kg2_querier = KG2Querier(log, self.plover_url)
         try:
-            answer_kg = kg2_querier.answer_one_hop_query(one_hop_qg)
+            answer_kg, ploverdb_duration = kg2_querier.answer_one_hop_query(one_hop_qg)
+            # Tuck the Plover query duration onto the Response for easy access
+            log.envelope["query_duration"] = {"ploverdb": ploverdb_duration}
         except Exception:
             tb = traceback.format_exc()
             error_type, error, _ = sys.exc_info()
